6eb624f13118a69f15ee33bb40d14e3a1902064b
Ben <maiahee@gmail.com>
2017-06-14 14:58:13 -0700

    seems to be a better method of aging ttable entries

--- src/minichess/TTableEntry.java
+++ src/minichess/TTableEntry.java
@@ -9,6 +9,10 @@ public class TTableEntry implements Serializable {
     int key;
     float size;
 
+    //the rate at which entries are reduced in value to allow entries that are very valuable but obsolete to be replaced
+    //entries will retain this percent of their value after a collision, tuned purely by trial and error
+    private static final float AGE_RATE = .99f;
+
     public TTableEntry(int key, int depth) {
         this.key = key;
         this.depth = ((byte)depth);
@@ -52,7 +56,7 @@ public class TTableEntry implements Serializable {
 
     public void age() {
         //we only use this size to weight values, so we can use it to age entries to keep the table fresh
-        size -= .25f;// seems like a good age rate, may be on the high side though
+        size *= AGE_RATE;// seems like a good age rate, may be on the high side though
     }
 
 }
